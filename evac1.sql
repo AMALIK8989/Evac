fe316054920d42bc1db3fb6d402a863debdd4d93
ATTA E RABI MALIK <105522520+AMALIK8989@users.noreply.github.com>
2024-04-27 20:55:07 +0500

    Add files via upload

--- evac1.sql
+++ evac1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2024 at 05:26 PM
+-- Generation Time: Apr 27, 2024 at 05:54 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -28,18 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `email`, `password`) VALUES
-(1, 'admin', 'admin@example.com', 'admin');
 
 -- --------------------------------------------------------
 
@@ -48,25 +41,50 @@ INSERT INTO `admin` (`id`, `username`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `child` (
-  `id` int(11) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `age` varchar(255) NOT NULL,
-  `gender` varchar(255) NOT NULL
+  `child_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `child_name` varchar(50) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `age` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hospital_list`
+-- Table structure for table `child_vaccination`
 --
 
-CREATE TABLE `hospital_list` (
-  `id` int(11) NOT NULL,
-  `hospital_name` varchar(100) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `phone_number` varchar(20) DEFAULT NULL,
-  `status` enum('available','not available') DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL
+CREATE TABLE `child_vaccination` (
+  `child_vaccination_id` int(11) NOT NULL,
+  `child_id` int(11) NOT NULL,
+  `vaccine_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital`
+--
+
+CREATE TABLE `hospital` (
+  `hospital_id` int(11) NOT NULL,
+  `hospital_name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `vaccine_availability` enum('Available','Unavailable') DEFAULT 'Available'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital_vaccination`
+--
+
+CREATE TABLE `hospital_vaccination` (
+  `hospital_vaccination_id` int(11) NOT NULL,
+  `hospital_id` int(11) NOT NULL,
+  `vaccine_name` varchar(100) NOT NULL,
+  `vaccination_status` enum('Available','Unavailable') DEFAULT 'Available'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -76,11 +94,65 @@ CREATE TABLE `hospital_list` (
 --
 
 CREATE TABLE `parent` (
-  `id` int(11) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `repassword` varchar(255) NOT NULL
+  `parent_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parent_booking`
+--
+
+CREATE TABLE `parent_booking` (
+  `parent_booking_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `hospital_id` int(11) NOT NULL,
+  `vaccine_id` int(11) NOT NULL,
+  `booking_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(11) NOT NULL,
+  `child_vaccination_id` int(11) NOT NULL,
+  `report_status` enum('Pending','Completed') DEFAULT 'Pending',
+  `report_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `request_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `hospital_id` int(11) NOT NULL,
+  `request_status` enum('Pending','Approved','Rejected') DEFAULT 'Pending',
+  `request_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaccination`
+--
+
+CREATE TABLE `vaccination` (
+  `child_id` int(11) NOT NULL,
+  `vaccine_id` int(11) NOT NULL,
+  `vaccination_date` date NOT NULL,
+  `vaccination_status` enum('Pending','Completed') DEFAULT 'Pending',
+  `hospital_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -91,7 +163,7 @@ CREATE TABLE `parent` (
 
 CREATE TABLE `vaccination_list` (
   `vaccine_id` int(11) NOT NULL,
-  `vaccine_name` varchar(255) NOT NULL
+  `vaccine_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -125,32 +197,198 @@ INSERT INTO `vaccination_list` (`vaccine_id`, `vaccine_name`) VALUES
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
 -- Indexes for table `child`
 --
 ALTER TABLE `child`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`child_id`),
+  ADD KEY `parent_id` (`parent_id`);
 
 --
--- Indexes for table `hospital_list`
+-- Indexes for table `child_vaccination`
 --
-ALTER TABLE `hospital_list`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `child_vaccination`
+  ADD PRIMARY KEY (`child_vaccination_id`),
+  ADD KEY `child_id` (`child_id`),
+  ADD KEY `vaccine_id` (`vaccine_id`);
+
+--
+-- Indexes for table `hospital`
+--
+ALTER TABLE `hospital`
+  ADD PRIMARY KEY (`hospital_id`);
+
+--
+-- Indexes for table `hospital_vaccination`
+--
+ALTER TABLE `hospital_vaccination`
+  ADD PRIMARY KEY (`hospital_vaccination_id`),
+  ADD KEY `hospital_id` (`hospital_id`);
+
+--
+-- Indexes for table `parent`
+--
+ALTER TABLE `parent`
+  ADD PRIMARY KEY (`parent_id`);
+
+--
+-- Indexes for table `parent_booking`
+--
+ALTER TABLE `parent_booking`
+  ADD PRIMARY KEY (`parent_booking_id`),
+  ADD KEY `parent_id` (`parent_id`),
+  ADD KEY `hospital_id` (`hospital_id`),
+  ADD KEY `vaccine_id` (`vaccine_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `child_vaccination_id` (`child_vaccination_id`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `parent_id` (`parent_id`),
+  ADD KEY `hospital_id` (`hospital_id`);
+
+--
+-- Indexes for table `vaccination`
+--
+ALTER TABLE `vaccination`
+  ADD KEY `child_id` (`child_id`),
+  ADD KEY `hospital_id` (`hospital_id`),
+  ADD KEY `vaccine_id` (`vaccine_id`);
+
+--
+-- Indexes for table `vaccination_list`
+--
+ALTER TABLE `vaccination_list`
+  ADD PRIMARY KEY (`vaccine_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `child`
 --
 ALTER TABLE `child`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `child_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `hospital_list`
+-- AUTO_INCREMENT for table `child_vaccination`
 --
-ALTER TABLE `hospital_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `child_vaccination`
+  MODIFY `child_vaccination_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hospital`
+--
+ALTER TABLE `hospital`
+  MODIFY `hospital_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hospital_vaccination`
+--
+ALTER TABLE `hospital_vaccination`
+  MODIFY `hospital_vaccination_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parent`
+--
+ALTER TABLE `parent`
+  MODIFY `parent_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parent_booking`
+--
+ALTER TABLE `parent_booking`
+  MODIFY `parent_booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vaccination_list`
+--
+ALTER TABLE `vaccination_list`
+  MODIFY `vaccine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `child`
+--
+ALTER TABLE `child`
+  ADD CONSTRAINT `child_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `parent` (`parent_id`);
+
+--
+-- Constraints for table `child_vaccination`
+--
+ALTER TABLE `child_vaccination`
+  ADD CONSTRAINT `child_vaccination_ibfk_1` FOREIGN KEY (`child_id`) REFERENCES `child` (`child_id`),
+  ADD CONSTRAINT `child_vaccination_ibfk_2` FOREIGN KEY (`vaccine_id`) REFERENCES `vaccination_list` (`vaccine_id`);
+
+--
+-- Constraints for table `hospital_vaccination`
+--
+ALTER TABLE `hospital_vaccination`
+  ADD CONSTRAINT `hospital_vaccination_ibfk_1` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`);
+
+--
+-- Constraints for table `parent_booking`
+--
+ALTER TABLE `parent_booking`
+  ADD CONSTRAINT `parent_booking_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `parent` (`parent_id`),
+  ADD CONSTRAINT `parent_booking_ibfk_2` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`),
+  ADD CONSTRAINT `parent_booking_ibfk_3` FOREIGN KEY (`vaccine_id`) REFERENCES `vaccination_list` (`vaccine_id`);
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`child_vaccination_id`) REFERENCES `child_vaccination` (`child_vaccination_id`);
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `parent` (`parent_id`),
+  ADD CONSTRAINT `request_ibfk_2` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`);
+
+--
+-- Constraints for table `vaccination`
+--
+ALTER TABLE `vaccination`
+  ADD CONSTRAINT `vaccination_ibfk_1` FOREIGN KEY (`child_id`) REFERENCES `child` (`child_id`),
+  ADD CONSTRAINT `vaccination_ibfk_2` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`),
+  ADD CONSTRAINT `vaccination_ibfk_3` FOREIGN KEY (`vaccine_id`) REFERENCES `vaccination_list` (`vaccine_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
